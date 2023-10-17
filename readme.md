24fa9fcd17f3b07c0684ccd8a3255567c9170990
HUYNH QUANG BAU <bau.hq@neo-lab.vn>
2023-10-17 13:54:53 +0700

    - Update ubuntu2204-install.sh

--- readme.md
+++ readme.md
@@ -4,5 +4,5 @@
 ```
 curl -O https://raw.githubusercontent.com/bauhuynhdev/scripts/master/ubuntu2204-install.sh
 chmod +x ubuntu2204-install.sh
-./openvpn-install.sh
+./ubuntu2204-install.sh
 ```
--- ubuntu2204-install.sh
+++ ubuntu2204-install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # shellcheck disable=SC2164
 
+export NEEDRESTART_MODE=a
+export DEBIAN_FRONTEND=noninteractive
+export DEBIAN_PRIORITY=critical
+
 # Common
 apt update
 apt upgrade -y
@@ -24,9 +28,12 @@ add-apt-repository ppa:qbittorrent-team/qbittorrent-stable -y
 apt update
 apt install qbittorrent-nox -y
 qbittorrent-nox -d
+# Login: admin/adminadmin
 
 # Puppeteer
 apt install -yq libgtk-3-dev libnotify-dev gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libnss3 lsb-release xdg-utils
 
 # Chromium
-npx @puppeteer/browsers install chromium -y
+#npx @puppeteer/browsers install chromium -y
+apt update
+apt install chromium-browser -y
